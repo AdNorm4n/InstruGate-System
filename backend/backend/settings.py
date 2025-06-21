7f7e3ee30b84a61bf6084073420217143714bb31
AdNorm4n <adriannorman@graduate.utm.my>
2025-06-22 01:09:04 +0800

    updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,6 +70,7 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -154,19 +155,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'backend' / 'static',  # For custom static files like favicon.ico
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Absolute path
 
-# Ensure Django finds static files from installed apps (e.g., admin)
+STATICFILES_DIRS = [
+    BASE_DIR / 'backend' / 'static',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 ]
-
-# Debug static file collection
-STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
